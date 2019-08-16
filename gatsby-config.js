b0687e32a8342d57f7a651f5fe159def712d0d3d
Johnny Choudhury-Lucas <johnny@exito.tech>
2019-08-16 12:22:39 -0400

    Integrate Contentful using dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,27 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 module.exports = {
   siteMetadata: {
-    title: 'éxito.tech',
+    title: process.env.SITE_TITLE,
     author: 'Johnny C-L'
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
@@ -18,6 +33,8 @@ module.exports = {
         path: `${__dirname}/src/`
       }
     },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     'gatsby-transformer-remark',
   ]
 }
